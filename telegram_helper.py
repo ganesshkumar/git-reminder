a6d14759c8adbea25c3b864c3cd7bfdb5e0163a7
Ganessh Kumar <rpganesshkumar@gmail.com>
2016-08-06 09:28:30 +0530

    Update telegram_helper.py

--- telegram_helper.py
+++ telegram_helper.py
@@ -8,11 +8,11 @@ user_id = int(os.environ["USER_ID"])
 def init_bot():
     global bot
     bot.update_bot_info().wait()
-    print "Bot " + bot.username + " is ready!"
+    print ("Bot " + bot.username + " is ready!")
 
 def send_message(message):
     global bot, user_id
-    print bot.send_message(user_id, message).wait()
+    print (bot.send_message(user_id, message).wait())
 
 def get_updates(last_update_id=0):
     updates = bot.get_updates(offset=last_update_id).wait()
